c54536906770715927f392f99f68c1082d914a90
Mohammed-Alattas <120325742+Mohammed-Alattas@users.noreply.github.com>
2023-01-14 21:27:26 +0300

    sst

--- server/register/admin.py
+++ server/register/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import student
+from .models import *
 
 admin.site.register(student)
+admin.site.register(Supervisor)
+admin.site.register(TrainingBody)
