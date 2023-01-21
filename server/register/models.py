de146cae215aa08ac7131c990fba10c33409e297
Mohammed-Alattas <120325742+Mohammed-Alattas@users.noreply.github.com>
2023-01-21 19:25:53 +0300

    db

--- server/register/models.py
+++ server/register/models.py
@@ -1,31 +1,38 @@
 from django.db import models
 
-
 class student(models.Model):
     Fullname = models.CharField(max_length=30)
     Phone_num = models.CharField(max_length=20)
     Email = models.EmailField(max_length=150)
     Password = models.CharField(max_length=35)
-    University_ID = models.IntegerField()
+    University_ID = models.IntegerField(primary_key=True)
     College = models.CharField(max_length=50)
-    # To show the name in the data base 
+    
+    # To show the name in the database 
     def __str__ (self):
         return self.Fullname
 
 class Supervisor(models.Model):
     Fullname = models.CharField(max_length=30)
-    Staff_ID = models.CharField(max_length=20)
+    Staff_ID = models.CharField(max_length=20, primary_key=True)
     Email = models.EmailField(max_length=150)
     Password = models.CharField(max_length=35)
     Department = models.CharField(max_length=50)
     College = models.CharField(max_length=50)
     Phone_num = models.CharField(max_length=20)
-    # To show the name in the data base 
+    # ------
+    # S_UID = models.ForeignKey('student',on_delete=models.CASCADE,null=True)
+    # ------
+    
+    # To show the name in the database 
     def __str__ (self):
         return self.Fullname
 
 class TrainingBody(models.Model):
-    Fullname = models.CharField(max_length=30)
+    #  (1054, "Unknown column 'register_trainingbody.id' in 'field list'")
+    #  يظهر الخطأ اذا اخترت (Training bodys) لكن ممكن اضافة حساب جديد
+    # I added a primary_key to (Fullname) and the problem went away, but (Fullname) cannot be (primary_key)
+    Fullname = models.CharField(max_length=20, primary_key=True)
     Email = models.EmailField(max_length=150)
     Password = models.CharField(max_length=35)
     Address = models.CharField(max_length=50)
@@ -33,9 +40,10 @@ class TrainingBody(models.Model):
     Phone_num = models.CharField(max_length=20)
     Position = models.CharField(max_length=50)
     Website = models.URLField(max_length=256)
-    # To show the name in the data base 
+    # To show the name in the database 
     def __str__ (self):
         return self.Fullname
+
 
 
 
